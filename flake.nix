f2c59a7507cd2d0d1da33694b560924d38a4fbcb
erikjwright <hey@erikwright.dev>
2025-02-16 18:49:54 +0100

    fix syntax error

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
 	};
-	outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+	outputs = { self, nixpkgs, flake-utils }:
 		flake-utils.lib.eachDefaultSystem
 		(system:
 			let
