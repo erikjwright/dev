91fbf77b3a02e17ce91525ed2bcd3b1c8f0e74fa
erikjwright <hey@erikwright.dev>
2025-02-16 18:40:35 +0100

    add rust

--- flake.nix
+++ flake.nix
@@ -1,3 +1,32 @@
 {
-  outputs = inputs: {};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          # 👇 new! note that it refers to the path ./rust-toolchain.toml
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            # 👇 we can just use `rustToolchain` here:
+            buildInputs = [ rustToolchain ];
+          };
+        }
+      );
 }
